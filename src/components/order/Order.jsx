680a534c895b3a8b6916750afd644359b7172f78
Mirlan <email>
2022-10-09 03:36:02 +0600

    ok

---
+++ src/components/order/Order.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from "./order.module.scss";
+
+const Order = () => {
+    return (
+        <div className={s.order}>
+           <h1>Han</h1>
+        </div>
+    );
+};
+
+export default Order;
--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Intro from '../../components/intro/Intro';
+import Order from '../../components/order/Order';
 
 const Main = () => {
     return (
         <>
             <Intro/>
-            
+            <Order/>
         </>
     );
 };
